17e0a0554c892a3c0ee17855a49d397e2cb590f1
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-04-08 21:13:20 -0400

    clean up

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, send_file
 from api import api
-import sqlite3
-import csv
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.register_blueprint(api, url_prefix='/api')
